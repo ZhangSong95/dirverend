bdd2d2a5074b262facc9d99e17d2df104c124f16
xkwg <757910395@qq.com>
2020-06-13 18:50:14 +0800

    添加车辆信息列表

---
+++ src/components/endTask.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  111111
+</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    // 这里存放数据
+    return {
+
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+
+</style>
