6ae326467eebc0e230dbc2e9b93ca8e6d3ee7e48
张松 <757910395@qq.com>
2020-06-14 14:07:05 +0800

    上传图片页面

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -231,10 +231,10 @@ export default {
       status: 0,
       // 上传图片框架
       fileList: [
-        { url: 'https://img.yzcdn.cn/vant/leaf.jpg' },
+        // { url: 'https://img.yzcdn.cn/vant/leaf.jpg' }
         // Uploader 根据文件后缀来判断是否为图片文件
         // 如果图片 URL 中不包含类型信息，可以添加 isImage 标记来声明
-        { url: 'https://cloud-image', isImage: true }
+        // { url: 'https://cloud-image', isImage: true }
       ],
       form: {},
       rules: {
